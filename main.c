0d5ee19288c8b7e2b81594f1abc5cebf85030bf3
KIRCHNER-BANNWART Luana <Luana.KIRCHNER-BANNWART@cpnv.ch>
2019-04-04 10:28:40 +0200

    Calculation with pointers, within a function

--- main.c
+++ main.c
@@ -3,17 +3,22 @@
 
 int main()
 {
-    int age = 10;
-    int *pointeur = &age;
+    int numero1 = 2;
+    int numero2=10;
 
-    printf("%d\n", *pointeur);
-    printf("%d\n", pointeur);
-    tripleAge(pointeur);
-    printf("%d\n", *pointeur);
+    int *pointeur = &numero1; //Declarer un pointeur avec la valeurs d'une variable
+
+    printf("valeur du numero 2: %d\n", numero2);
+    printf("Valeur du numero 1: %d\n", *pointeur);
+    printf("L'adresse de la variable numero 1: %d\n", pointeur); //aficher l'adresse où la varible est stoque sur la memoire RAM
+
+    Calcule_avec_pointeru(pointeur, &numero2); //Deux maniere de mettre les valeurs des pointeurs dans une focntion;
+    printf("Numero 2 - numero 1 = %d\n Numero 1 * Numero 2 (apres sa modification): %d \n", numero2, *pointeur); //Affiche les valeurs des pointers aprés son calcule.
 }
 
-void tripleAge(int *pointeurSurAge){
+void Calcule_avec_pointeru(int *pointeurN1, int *pointeurN2){
 
-*pointeurSurAge *=3;
+*pointeurN2 = *pointeurN2 - *pointeurN1; //resultat 8
+*pointeurN1 = *pointeurN1 * *pointeurN2; //resultat 16
 
 }
