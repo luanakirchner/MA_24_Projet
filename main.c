b477402cfc179d4ffb28c586820a367e1e36427a
KIRCHNER-BANNWART Luana <Luana.KIRCHNER-BANNWART@cpnv.ch>
2019-04-04 11:50:33 +0200

    Calculation with comma

--- main.c
+++ main.c
@@ -1,52 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void Calcule_avec_pointeru(int *pointeurN1, int *pointeurN2);
+void calcul(int *Choix, float *N1, float *N2, float *resultad, float *resultad_inverse);
 
-int main()
-{
-    int numero1 = 2;
-    int numero2=10;
 
-    int *pointeur = &numero1; //Declarer un pointeur avec la valeurs d'une variable
+int main(){
 
-    printf("valeur du numero 2: %d\n", numero2);
-    printf("Valeur du numero 1: %d\n", *pointeur);
-    printf("L'adresse de la variable numero 1: %d\n", pointeur); //aficher l'adresse où la varible est stoque sur la memoire RAM
+//Probleme avec les doubles et pointeur.
 
-    Calcule_avec_pointeru(pointeur, &numero2); //Deux maniere de mettre les valeurs des pointeurs dans une focntion;
-    printf("Numero 2 - numero 1 = %d\n Numero 1 * Numero 2 (apres sa modification): %d \n", numero2, *pointeur); //Affiche les valeurs des pointers aprés son calcule.
-
-    mini_calculatrice();
-}
-
-void Calcule_avec_pointeru(int *pointeurN1, int *pointeurN2){
-
-*pointeurN2 = *pointeurN2 - *pointeurN1; //resultat 8
-*pointeurN1 = *pointeurN1 * *pointeurN2; //resultat 16
-
-}
-
-void mini_calculatrice(){
-
-int premierN1 = 0;
-int deuxiemeN2 = 0;
+float premierN1 = 0;
+float deuxiemeN2 = 0;
 int choix=0;
-
-int *pointeurpremiereN1 = NULL;
-int *pointeurpremiereN2 = NULL;
-int resultad = 0;
+float *pointeurpremierN1 = NULL;
+float *pointeurpremierN2 = NULL;
+float resultad = 0;
+float resultad_inverse = 0;
 
 
 printf("\nEntrez un numero ");
-scanf("%d", &premierN1);
+scanf("%f", &premierN1);
 printf("\nEntrez un deuxieme numero ");
-scanf("%d",&deuxiemeN2);
+scanf("%f",&deuxiemeN2);
 
 printf("\nQuelle option de calcul voulez vous?\n");
 printf("1 - Multiplication\n");
 printf("2 - Addition\n");
 printf("3 - Soustration\n");
+printf("4 - Division\n");
 scanf("%d" ,&choix);
 
 
@@ -54,28 +34,35 @@ pointeurpremierN1 = &premierN1; // Recuperer les valeurs entree par l'utilisateu
 pointeurpremierN2 = &deuxiemeN2;
 
 
-printf("Vous avez entree : %d et %d \n", *pointeurpremiereN1, *pointeurpremiereN2); // Certifier que les pointeurs a les bonnes valeurs
+printf("Vous avez entree : %.2f et %.2f \n", *pointeurpremierN1, *pointeurpremierN2); // Certifier que les pointeurs a les bonnes valeurs
 
-calcul(&choix, &premierN1, &deuxiemeN2, &resultad);
-printf("\n%d", resultad);
+calcul(&choix, &premierN1, &deuxiemeN2, &resultad ,&resultad_inverse);
+printf("\nLe resultat de l'operation est :%.2f\n", resultad);
+printf("Le resultat de l'operation a l'inverse est :%.2f\n", resultad_inverse);
 
 
 }
 
-void calcul(int *Choix, int *N1, int *N2, int *resultad){
+void calcul(int *Choix, float *N1, float *N2, float *resultad, float *resultad_inverse){
 
-printf("\n%d %d %d %d\n", *Choix, *N1, *N2, *resultad); // Certifier que les pointeurs a les bonnes valeurs
+//printf("\n%d %f %f %f\n", *Choix, *N1, *N2, *resultad); // Certifier que les pointeurs a les bonnes valeurs
 
 switch(*Choix){
-case 1: *resultad = *N1 * *N2;
-        break;
 
-case 2: *resultad = *N1 + *N2;
-        break;
+    case 1: *resultad = *N1 * *N2;
+            *resultad_inverse = *N2 * *N1;
+            break;
 
-case 3: *resultad = *N1 - *N2;
-        break;
+    case 2: *resultad = *N1 + *N2;
+            *resultad_inverse = *N2 + *N1;
+            break;
 
-}
+    case 3: *resultad = *N1 - *N2;
+            *resultad_inverse = *N2 - *N1;
+            break;
+    case 4: *resultad = *N1 / *N2;
+            *resultad_inverse = *N2 / *N1;
+            break;
+    }
 
 }
