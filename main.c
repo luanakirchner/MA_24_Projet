22ed18de343ed639ed5bc0c5aa0df5b1dfaa2c79
KIRCHNER-BANNWART Luana <Luana.KIRCHNER-BANNWART@cpnv.ch>
2019-04-04 10:50:34 +0200

    Test: value entered by user, retrieved on a pointer

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void Calcule_avec_pointeru(int *pointeurN1, int *pointeurN2);
+
 int main()
 {
     int numero1 = 2;
@@ -14,11 +16,34 @@ int main()
 
     Calcule_avec_pointeru(pointeur, &numero2); //Deux maniere de mettre les valeurs des pointeurs dans une focntion;
     printf("Numero 2 - numero 1 = %d\n Numero 1 * Numero 2 (apres sa modification): %d \n", numero2, *pointeur); //Affiche les valeurs des pointers aprés son calcule.
+
+    mini_calculatrice();
 }
 
 void Calcule_avec_pointeru(int *pointeurN1, int *pointeurN2){
 
 *pointeurN2 = *pointeurN2 - *pointeurN1; //resultat 8
 *pointeurN1 = *pointeurN1 * *pointeurN2; //resultat 16
+
+}
+
+void mini_calculatrice(){
+
+int premierN1 = 0;
+int deuxiemeN2 = 0;
+
+int *pointeurpremiereN1 = NULL;
+int *pointeurpremiereN2 = NULL;
+
+printf("\nEntrez un numero ");
+scanf("%d", &premierN1);
+printf("\nEntrez un deuxieme numero ");
+scanf("%d",&deuxiemeN2);
+
+pointeurpremiereN1 = &premierN1; // Recuperer les valeurs entrer par l'utilisateur dans un pointeur
+pointeurpremiereN2 = &deuxiemeN2;
+
+printf("Vous avez entree : %d et %d \n", *pointeurpremiereN1, *pointeurpremiereN2); // Certifier que les pointeurs a les bonnes valeurs
+
 
 }
