8414e1934d3b335c29809ec4b297353781e97cdb
KIRCHNER-BANNWART Luana <Luana.KIRCHNER-BANNWART@cpnv.ch>
2019-04-04 11:20:55 +0200

    First try of a calculator

--- main.c
+++ main.c
@@ -31,19 +31,51 @@ void mini_calculatrice(){
 
 int premierN1 = 0;
 int deuxiemeN2 = 0;
+int choix=0;
 
 int *pointeurpremiereN1 = NULL;
 int *pointeurpremiereN2 = NULL;
+int resultad = 0;
+
 
 printf("\nEntrez un numero ");
 scanf("%d", &premierN1);
 printf("\nEntrez un deuxieme numero ");
 scanf("%d",&deuxiemeN2);
 
-pointeurpremiereN1 = &premierN1; // Recuperer les valeurs entrer par l'utilisateur dans un pointeur
-pointeurpremiereN2 = &deuxiemeN2;
+printf("\nQuelle option de calcul voulez vous?\n");
+printf("1 - Multiplication\n");
+printf("2 - Addition\n");
+printf("3 - Soustration\n");
+scanf("%d" ,&choix);
+
+
+pointeurpremierN1 = &premierN1; // Recuperer les valeurs entree par l'utilisateur dans un pointeur
+pointeurpremierN2 = &deuxiemeN2;
+
 
 printf("Vous avez entree : %d et %d \n", *pointeurpremiereN1, *pointeurpremiereN2); // Certifier que les pointeurs a les bonnes valeurs
 
+calcul(&choix, &premierN1, &deuxiemeN2, &resultad);
+printf("\n%d", resultad);
+
+
+}
+
+void calcul(int *Choix, int *N1, int *N2, int *resultad){
+
+printf("\n%d %d %d %d\n", *Choix, *N1, *N2, *resultad); // Certifier que les pointeurs a les bonnes valeurs
+
+switch(*Choix){
+case 1: *resultad = *N1 * *N2;
+        break;
+
+case 2: *resultad = *N1 + *N2;
+        break;
+
+case 3: *resultad = *N1 - *N2;
+        break;
+
+}
 
 }
